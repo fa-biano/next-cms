a3b9a2a4e68112c66c21f4d88225326683199e46
Fa-biano <fbiano.souza@gmail.com>
2025-06-15 17:31:20 -0300

    feat: create home submenu component

---
+++ src/components/home/subMenu/index.tsx
@@ -0,0 +1,44 @@
+'use client'
+import Link from 'next/link'
+import styles from './styles.module.scss'
+import { X, Menu } from 'lucide-react'
+import { useState, useEffect } from 'react'
+
+export function Submenu() {
+  const [isOpen, setIsOpen] = useState(true)
+    
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) setIsOpen(false)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return(
+    <section className={styles.submenu}>
+      <div className={styles.submenuIcon} onClick={toggleMenu}>
+        <Menu size={34} color='#121212' />
+        Serviços
+      </div>
+
+      <ul className={`${styles.ul} ${isOpen ? styles.open : ''}`}>
+        { isOpen && (
+          <button onClick={toggleMenu} className={styles.closeButton}>
+            <X size={54} color="#121212" />
+          </button>
+        )}
+
+        <li>
+          <Link href='/post/pagina-1'> Pagina 1 </Link>
+          <Link href='/post/pagina-2'> Pagina 2 </Link>
+        </li>
+      </ul>
+    </section>
+  )
+}
