5a5345a9cd56e029297e767f0e5f6e30ffe65821
Fa-biano <fbiano.souza@gmail.com>
2025-06-20 17:38:47 -0300

    feat: add Hero and Services component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
+import { Container } from '@/components/container'
+import Hero from '@/components/hero'
+import { Services } from '@/components/home/services'
 import { Submenu } from '@/components/home/subMenu'
+import { getHomeData } from '@/utils/actions/cosmicjs'
+import { Phone } from 'lucide-react'
 
-export default function Home() {
+export default async function Home() {
+  const { object } = await getHomeData()
+  
   return (
     <main>
       <Submenu />
+      <Hero 
+        heading={object.metadata.heading}
+        buttonTitle={object.metadata.cta_button.title}
+        buttonUrl={object.metadata.cta_button.url}
+        bannerUrl={object.metadata.banner.url} 
+        icon={<Phone size={24} color="#FFF" />}
+      />
+
+      <Container>
+        <Services object={object} />
+      </Container>
     </main>
   )
 }
