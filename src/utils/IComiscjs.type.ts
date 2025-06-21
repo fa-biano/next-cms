eb3d2902d43fa05db0e8a1ea6bc8d1ab519fae03
Fa-biano <fbiano.souza@gmail.com>
2025-06-21 15:12:30 -0300

    feat: add ICosmicJSPageDetail to types

--- src/utils/IComiscjs.type.ts
+++ src/utils/IComiscjs.type.ts
@@ -47,3 +47,34 @@ interface ISubmenu {
 export interface ICosmicJSMenu {
   objects: ISubmenu[]
 }
+
+interface PageDetail {
+  slug: string
+  title: string
+  content: string
+  metadata: {
+    banner: {
+      url: string
+      imgix_url: string
+    }
+    button: {
+      title: string
+      url: string
+    }
+    description: {
+      title: string
+      text: string
+      banner: {
+        url: string
+        imgix_url: string
+      }
+      button_active: boolean
+      button_title: string
+      button_url: string
+    }
+  }
+}
+
+export interface ICosmicJSPageDetail {
+  objects: PageDetail[]
+}
