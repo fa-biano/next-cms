d018d46f4bb7c0d81c98d4b3f6033a4929ab5abe
Fa-biano <fbiano.souza@gmail.com>
2025-06-21 15:39:42 -0300

    feat: create post/[slug] page

---
+++ src/app/post/[slug]/page.tsx
@@ -0,0 +1,60 @@
+import { getPageBySlug } from '@/utils/actions/cosmicjs'
+import styles from './styles.module.scss'
+import Hero from '@/components/hero'
+import { Phone } from 'lucide-react'
+import { Container } from '@/components/container'
+import Image from 'next/image'
+
+export default async function Page({ params }: { params: Promise<{ slug: string }> }){
+  const { slug } = await params
+  const { objects } = await getPageBySlug(slug)
+
+  return(
+    <>
+      <Hero
+        heading={objects[0].title}
+        buttonTitle={objects[0].metadata.button.title}
+        buttonUrl={objects[0].metadata.button.url}
+        bannerUrl={objects[0].metadata.banner.url}
+        icon={<Phone size={24} color="#FFF" />}
+      />
+
+      <Container>
+        <section className={styles.about}>
+
+          <article className={styles.innerAbout}>
+            <h1 className={styles.title}>
+              {objects[0].metadata.description.title}
+            </h1>
+            <p>
+              {objects[0].metadata.description.text}
+            </p>
+
+            {objects[0].metadata.description.button_active && (
+              <a
+                href={objects[0].metadata.description.button_url as string}
+                target='_blank'
+                className={styles.link}
+              >
+                {objects[0].metadata.description.button_title}
+              </a>
+            )}
+          </article>
+
+          <div className={styles.bannerAbout}>
+            <Image
+              className={styles.imageAbout}
+              alt={objects[0].title}
+              quality={100}
+              fill={true}
+              priority={true}
+              sizes='(max-width: 480px) 100vw, (max-width: 1024px) 75vw, 60vw'
+              src={objects[0].metadata.description.banner.url}
+            />
+          </div>
+
+        </section>
+      </Container>
+    </>
+  )
+}
